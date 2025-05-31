a3e35c62a36bc535b5e65c9cc975043a577e2e8d
ixally <xallmean@gmail.com>
2025-05-31 14:28:12 +0700

    Update fsubbot.py

--- fsubbot.py
+++ fsubbot.py
@@ -4,7 +4,19 @@ import requests
 from pyrogram import Client
 from pyrogram.errors import FloodWait
 
-app = Client("session")  # atau tambahin param lain sesuai bot kamu
+logging.basicConfig(
+    level=logging.WARNING, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+app = Client(
+    ":memory:",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    plugins=dict(root="ForceSubscribeBot"),
+)
 
 def sync_time():
     try:
